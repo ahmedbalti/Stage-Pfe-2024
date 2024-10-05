4172c218830fc9c2c8c75947a5e24511ccc98d1c
Ahmed <ahmed.balti@esprit.tn>
2024-10-05 23:52:34 +0100

    adding nexus to the pipeline

--- BackEnd/Gestion User/Gestion User/obj/Debug/net6.0/Gestion User.AssemblyInfo.cs
+++ BackEnd/Gestion User/Gestion User/obj/Debug/net6.0/Gestion User.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestion User")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+080cbe8cb2cc8c020f39c03432ab3435dc5708cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5af9a5ebe0bcbf06442288f4f806e394db2d1b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestion User")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestion User")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
