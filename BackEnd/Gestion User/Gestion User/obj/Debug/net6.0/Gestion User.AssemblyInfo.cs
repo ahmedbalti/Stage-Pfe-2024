935290e15b9cb47d981e2c4df959a442c2e291f5
Ahmed <ahmed.balti@esprit.tn>
2024-10-07 12:44:33 +0100

    adding nexus to the pipeline

--- BackEnd/Gestion User/Gestion User/obj/Debug/net6.0/Gestion User.AssemblyInfo.cs
+++ BackEnd/Gestion User/Gestion User/obj/Debug/net6.0/Gestion User.AssemblyInfo.cs
@@ -12,11 +12,11 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("245adab4-22f5-41df-b878-f1441b0aa339")]
-[assembly: System.Reflection.AssemblyCompanyAttribute("YourName")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("AhmedBalti")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Gestion User Project for .NET 6")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee3d11f020ccb454b66e80573d2e014198fc733")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca1f1e79ceee4e1fcba546bf4f3c38d704b827c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestion User")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestion User")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
